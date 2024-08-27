882e9f8b6a14879b90abec4f2992ca68c7d3cccd
JabssDev <jordanburgoss23@gmail.com>
2024-08-27 11:14:33 -0500

    feat: :zap: Change the direction of the toggle button for dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,19 @@
 export const Header = () => {
+	const handleClickTheme = () => {
+		document.body.classList.toggle("dark");
+	};
+
 	return (
-		<header className="bg-lt-very-pale-blue h-56 p-5 rounded-b-3xl">
-			<h1 className="text-dt-very-dark-blue font-bold text-2xl mt-3 leading-9">Social Media Dashboard</h1>
-			<p className="font-bold text-sm text-lt-dark-grayish-blue">Total Followers: 23,004</p>
-			<hr className="my-5 border-lt-dark-grayish-blue" />
+		<header className="bg-ct-very-pale-blue dark:bg-ct-dark-blue-top h-56 p-5 rounded-b-3xl">
+			<h1 className="text-ct-very-dark-blue dark:text-ct-very-pale-blue font-bold text-2xl mt-3 leading-9">Social Media Dashboard</h1>
+			<p className="font-bold text-sm text-ct-dark-grayish-blue dark:text-ct-desaturated-blue">Total Followers: 23,004</p>
+			<hr className="my-5 border-ct-dark-grayish-blue dark:border-ct-desaturated-blue" />
 			<div className="flex justify-between">
-				<p className="text-sm font-bold text-lt-dark-grayish-blue">Dark Mode</p>
-				<label className="border bg-dt-toogle w-11 h-6 rounded-full overflow-hidden cursor-pointer p-[3px] relative" htmlFor="toogle-theme">
-					<input className="peer sr-only" type="checkbox" id="toogle-theme" />
-					<div className="w-full h-full peer-checked:bg-toogle absolute top-0 left-0"></div>
-					<div className="w-4 h-4 bg-white rounded-full peer-checked:translate-x-5 transition-all"></div>
+				<p className="text-sm font-bold text-ct-dark-grayish-blue dark:text-ct-desaturated-blue">Dark Mode</p>
+				<label className=" bg-ct-toogle w-[46px] h-6 rounded-full overflow-hidden cursor-pointer p-[4px] relative" htmlFor="toogle-theme">
+					<input className="peer sr-only" type="checkbox" id="toogle-theme" onClick={handleClickTheme} />
+					<div className="w-full h-full peer-checked:bg-toogle-gradient absolute top-0 left-0"></div>
+					<div className="w-4 h-4 bg-white dark:bg-ct-dark-blue-top rounded-full peer-checked:translate-x-[-23px] transition-all absolute right-1"></div>
 				</label>
 			</div>
 		</header>
