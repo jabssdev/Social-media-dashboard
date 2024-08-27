971ce364b4bca5d018b9bb5b1e8e25127bece0d1
JabssDev <jordanburgoss23@gmail.com>
2024-08-27 10:48:15 -0500

    style: :technologist: Improve custom color settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+	darkMode: "class",
 	content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		extend: {
 			colors: {
-				primary: {
+				ct: {
 					"lime-green": "hsl(163, 72%, 41%)",
 					"bright-red": "hsl(356, 69%, 56%)",
 					facebook: "hsl(208, 92%, 53%)",
 					twitter: "hsl(203, 89%, 53%)",
 					youtube: "hsl(348, 97%, 39%)",
-				},
-				dt: {
 					toogle: "hsl(230, 22%, 74%)",
 					"very-dark-blue": "hsl(230, 17%, 14%)",
 					"dark-blue-top": "hsl(232, 19%, 15%)",
 					"dark-desaturated-blue": "hsl(228, 28%, 20%)",
 					"desaturated-blue": "hsl(228, 34%, 66%)",
-				},
-				lt: {
 					"very-pale-blue": "hsl(225, 100%, 98%)",
 					"light-grayish-blue": "hsl(227, 47%, 96%)",
 					"dark-grayish-blue": "hsl(228, 12%, 44%)",
@@ -28,7 +25,7 @@ export default {
 				sans: ["Inter", "sans-serif"],
 			},
 			backgroundImage: {
-				toogle: "linear-gradient(to right, hsl(210, 78%, 56%), hsl(146, 68%, 55%))",
+				"toogle-gradient": "linear-gradient(to right, hsl(210, 78%, 56%), hsl(146, 68%, 55%))",
 				instagram: "linear-gradient(to right, hsl(37, 97%, 70%), hsl(329, 70%, 58%))",
 			},
 		},
