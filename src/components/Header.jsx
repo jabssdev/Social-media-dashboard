73643d7bd5526e891ea1fb30c7bb8974ac20d4c4
JabssDev <jordanburgoss23@gmail.com>
2024-08-28 19:06:48 -0500

    feat: :sparkles: Modify function to handle dark mode in the document

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 export const Header = () => {
 	const handleClickTheme = () => {
-		document.body.classList.toggle("dark");
+		document.documentElement.classList.toggle("dark");
 	};
 
 	return (
-		<header className="bg-ct-very-pale-blue dark:bg-ct-dark-blue-top h-56 p-5 rounded-b-3xl">
+		<header className="bg-ct-very-pale-blue dark:bg-ct-dark-blue-top h-60 p-5 rounded-b-3xl">
 			<h1 className="text-ct-very-dark-blue dark:text-ct-very-pale-blue font-bold text-2xl mt-3 leading-9">Social Media Dashboard</h1>
 			<p className="font-bold text-sm text-ct-dark-grayish-blue dark:text-ct-desaturated-blue">Total Followers: 23,004</p>
 			<hr className="my-5 border-ct-dark-grayish-blue dark:border-ct-desaturated-blue" />
